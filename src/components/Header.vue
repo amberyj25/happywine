0fc184c1b3b7d29d3353c208b20fbbef4d49aeb2
amber <amber20240@gmail.com>
2020-09-24 22:06:57 +0800

    happy wine

---
+++ src/components/Header.vue
@@ -0,0 +1,191 @@
+<template>
+  <header>
+    <div class="carousel">
+        <!-- Bootstrap -->
+        <!-- navbar-expand-xl 從這改漢堡在xl以上不出現 -->
+          <nav class="navbar navbar-expand-xl bg-light" id="header">
+            <div>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"><i class="fas fa-bars menu_color"></i></span>
+              </button>
+            </div>
+
+            <div class="logo w-xl-50">
+              <h1>
+                <a href="##">Happy Wine</a>
+              </h1>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">SHOP<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">PROMITION</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">EVENTS</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">ABOUT US</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">CONTACT</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Login</a>
+                    </li>
+                </ul>
+              </div>
+            </div>
+
+          </nav>
+      
+      <div>
+        <b-carousel
+          id="carousel-1"
+          v-model="slide"
+          :interval="2000"
+          controls
+          indicators
+          background="#ababab"
+          img-width="1024"
+          img-height="480"
+          style="text-shadow: 1px 1px 2px #333;"
+          @sliding-start="onSlideStart"
+          @sliding-end="onSlideEnd"
+        >
+          <!-- Text slides with image -->
+          <b-carousel-slide img-src="https://i.imgur.com/6x0J234.jpg" style="height:720px"></b-carousel-slide>
+
+          <!-- Slides with custom text -->
+          <b-carousel-slide img-src="https://i.imgur.com/TBHJmjv.jpg" style="height:720px"></b-carousel-slide>
+
+          <!-- Slides with image only -->
+          <b-carousel-slide img-src="https://i.imgur.com/CQyNAxw.jpg" style="height:720px"></b-carousel-slide>
+        </b-carousel>
+      </div>
+      
+    </div>
+  </header>
+
+</template>
+
+<script>
+export default{
+  name:"Rooms",
+    methods: {
+      onSlideStart(slide) {
+        this.sliding = true
+      },
+      onSlideEnd(slide) {
+        this.sliding = false
+      },
+    }
+}
+</script>
+<style scoped>
+#carousel-1{
+  position:relative;
+}
+/* 以下ｏｋ */
+#header{
+  width:100%;
+  position:absolute;
+  top:0;
+  left:0;
+  z-index:2;
+  background-color:#494b50 !important;
+  padding: 20px;
+  opacity:0.9;
+}
+header nav{
+  color:white;
+  padding: 25px 50px !important;
+  font-size: 25px;
+}
+header button i{
+  font-size: 25px;
+  color: 	#AAAAAA;
+}
+header ul a{
+  font-size: 1rem;
+}
+header .logo {
+  width:100%;
+}
+header h1 {
+  text-align:center;
+}
+header .logo a{
+  display: block;
+  color: white;
+}
+
+header .logo img{
+  display:block;
+  margin: auto;
+}
+header .logo ul{
+  margin: 5px auto;
+}
+header .logo ul li{
+  padding: 0 10px;
+  margin: 0 10px
+}
+header .shopping ul{
+  margin-left: auto;
+}
+header .shopping ul a{
+  color:white;
+}
+/* 以上ｏｋ */
+
+.header .logo img{
+  width:25%;
+  margin: auto;
+  display: block;
+}
+.header .logo .navbar nav a{
+  display: inline-block;
+  margin: 5px 25px ;
+  text-decoration: none;
+  color: white;
+}
+.navbar a:hover{
+    font-weight:bold;
+}
+.header .shopping{
+  display: flex;
+}
+.header .shopping a{
+  text-decoration:none;
+  color:white;
+}
+.header .shopping :nth-child(2){
+  margin: 0 20px;
+}
+
+header nav .n12{
+  background-color:blue;
+}
+/* media query*/
+@media (max-width: 1200px) {
+    .header .logo img{
+      width:50%;
+    }
+    #header{
+    padding: 10px !important;
+}
+  }
+
+@media (max-width: 992px) {
+    header .logo img{
+      width: 50px;
+    }
+  }
+@media (max-width: 576px) {
+    header .logo img{
+      width: 25px;
+    }
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Vuex from "vuex"
+import store from "./store"
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import App from './App'
+
+Vue.use(Vuex);
+Vue.use(VueAxios, axios)
+Vue.use(BootstrapVue)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  components: { App },
+  template: '<App/>'
+})
