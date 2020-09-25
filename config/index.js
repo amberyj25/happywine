d445e15ee5ae7753d6cbf0835fb3ab73abae341d
amber <amber20240@gmail.com>
2020-09-25 12:38:06 +0800

    update github pages 路徑

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/wine_happySpace/dist/',
+    assetsPublicPath: '/happywine/dist/',
 
     /**
      * Source Maps
