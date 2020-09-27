89e3d8af13d0de02814923754494eaf1f3f5f19a
amber <amber20240@gmail.com>
2020-09-27 13:01:55 +0800

    修改navbar 和 navbar style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,45 +1,32 @@
 <template>
   <header>
     <div class="carousel">
-        <!-- Bootstrap -->
-        <!-- navbar-expand-xl 從這改漢堡在xl以上不出現 -->
-          <nav class="navbar navbar-expand-xl bg-light" id="header">
-            <div>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"><i class="fas fa-bars menu_color"></i></span>
-              </button>
-            </div>
-
-            <div class="logo w-xl-50">
+        <div>
+          <b-navbar toggleable="lg" type="dark" variant="info" class="header_navabar">
+            <b-navbar-brand>
               <h1>
-                <a href="##">Wine Space</a>
+                <router-link to="/">Wine Space</router-link>
               </h1>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                      <a class="nav-link" href="#">SHOP<span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">PROMITION</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">EVENTS</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">ABOUT US</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">CONTACT</a>
-                    </li>
-                    <li class="nav-item">
-                      <router-link to="/Login" v-show="logInOrOut == false">Login</router-link>
-                      <p v-show="logInOrOut == true" @click="logOut">Logout</p>
-                    </li>
-                </ul>
-              </div>
-            </div>
+            </b-navbar-brand>
 
-          </nav>
+            <b-navbar-toggle target="nav-collapse"><i class="fas fa-bars menu_color"></i></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+              <b-navbar-nav class="ml-auto navbar_right">
+                <b-nav-item right>
+                  <router-link to="/">首頁</router-link>
+                </b-nav-item>
+                <b-nav-item right>
+                  <router-link to="/products">商品</router-link>
+                </b-nav-item>
+                <b-nav-item right>
+                  <router-link to="/login" v-show="logInOrOut == false">Login</router-link>
+                  <p v-show="logInOrOut == true" @click="logOut">Logout</p>
+                </b-nav-item>
+              </b-navbar-nav>
+            </b-collapse>
+          </b-navbar>
+        </div>
       
       <div>
         <b-carousel
@@ -90,87 +77,36 @@ export default{
 }
 </script>
 <style scoped>
-/* 以下ｏｋ */
-#header{
+.header_navabar{
   width:100%;
   position:absolute;
   top:0;
   left:0;
   z-index:2;
   background-color:#494b50 !important;
-  padding: 20px;
+  padding: 10px 100px;
   opacity:0.9;
 }
-header nav{
+.header_navabar a{
   color:white;
-  padding: 25px 50px !important;
-  font-size: 25px;
-}
-header button i{
-  font-size: 25px;
-  color: 	#AAAAAA;
-}
-header ul a{
-  font-size: 1rem;
-}
-header .logo {
-  width:100%;
-}
-header h1 {
-  text-align:center;
-}
-header .logo a{
-  display: block;
-  color: white;
-}
-
-header .logo img{
-  display:block;
-  margin: auto;
-}
-header .logo ul{
-  margin: 5px auto;
-}
-header .logo ul li{
-  padding: 0 10px;
-  margin: 0 10px
-}
-header .shopping ul{
-  margin-left: auto;
-}
-header .shopping ul a{
-  color:white;
-}
-/* 以上ｏｋ */
-
-.header .logo img{
-  width:25%;
-  margin: auto;
-  display: block;
-}
-.header .logo .navbar nav a{
-  display: inline-block;
-  margin: 5px 25px ;
   text-decoration: none;
+}
+.header_navabar h1 a{
+  font-size:3rem;
+}
+.header_navabar button i{
+  font-size: 25px;
   color: white;
 }
-.navbar a:hover{
+
+.header_navabar .navbar_right a{
+  margin:0 20px;
+  font-size: 1.4rem;
+}
+#header a:hover{
     font-weight:bold;
 }
-.header .shopping{
-  display: flex;
-}
-.header .shopping a{
-  text-decoration:none;
-  color:white;
-}
-.header .shopping :nth-child(2){
-  margin: 0 20px;
-}
 
-header nav .n12{
-  background-color:blue;
-}
 #carousel-1{
   position:relative;
 }
@@ -179,25 +115,18 @@ header nav .n12{
 }
 /* media query*/
 @media (max-width: 1200px) {
-    .header .logo img{
-      width:50%;
-    }
-    #header{
-      padding: 10px !important;
+    .header_navabar{
+      padding: 10px 50px;
     }
     .carousel_img{
       height:50%;
     }
   }
-
+    /* 
 @media (max-width: 992px) {
-    header .logo img{
-      width: 50px;
-    }
   }
 @media (max-width: 576px) {
-    header .logo img{
-      width: 25px;
-    }
+
   }
+  */
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from "@/App.vue"
 import Login from "@/components/Login.vue"
+import Products from "@/components/Products.vue"
 
 
 Vue.use(VueRouter)
@@ -13,7 +14,11 @@ export default new VueRouter({
       name: "App",
       component: App,
     },{
-      path: "/Login",
+      path: "/products",
+      name: "Products",
+      component: Products,
+    },{
+      path: "/login",
       name: "Login",
       component: Login,
     }
