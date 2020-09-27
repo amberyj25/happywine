bc4e537dac235540061fc9e9a2c401d83cc00134
amber <amber20240@gmail.com>
2020-09-27 15:45:54 +0800

    add navbar component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,7 @@
 <template>
   <header>
     <div class="carousel">
-        <div>
-          <b-navbar toggleable="lg" type="dark" variant="info" class="header_navabar">
-            <b-navbar-brand>
-              <h1>
-                <router-link to="/">Wine Space</router-link>
-              </h1>
-            </b-navbar-brand>
-
-            <b-navbar-toggle target="nav-collapse"><i class="fas fa-bars menu_color"></i></b-navbar-toggle>
-
-            <b-collapse id="nav-collapse" is-nav>
-              <b-navbar-nav class="ml-auto navbar_right">
-                <b-nav-item right>
-                  <router-link to="/">首頁</router-link>
-                </b-nav-item>
-                <b-nav-item right>
-                  <router-link to="/products">商品</router-link>
-                </b-nav-item>
-                <b-nav-item right>
-                  <router-link to="/login" v-show="logInOrOut == false">Login</router-link>
-                  <p v-show="logInOrOut == true" @click="logOut">Logout</p>
-                </b-nav-item>
-              </b-navbar-nav>
-            </b-collapse>
-          </b-navbar>
-        </div>
+      <navbar></navbar>
       
       <div>
         <b-carousel
@@ -59,8 +34,13 @@
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue"
+
 export default{
   name:"Header",
+  components:{
+    Navbar
+  },
     methods: {
       onSlideStart(slide) {
         this.sliding = true
@@ -68,44 +48,10 @@ export default{
       onSlideEnd(slide) {
         this.sliding = false
       },
-    },
-    computed:{
-      logInOrOut(){
-        return this.$store.state.signinChange;
-      }
     }
 }
 </script>
 <style scoped>
-.header_navabar{
-  width:100%;
-  position:absolute;
-  top:0;
-  left:0;
-  z-index:2;
-  background-color:#494b50 !important;
-  padding: 10px 100px;
-  opacity:0.9;
-}
-.header_navabar a{
-  color:white;
-  text-decoration: none;
-}
-.header_navabar h1 a{
-  font-size:3rem;
-}
-.header_navabar button i{
-  font-size: 25px;
-  color: white;
-}
-
-.header_navabar .navbar_right a{
-  margin:0 20px;
-  font-size: 1.4rem;
-}
-#header a:hover{
-    font-weight:bold;
-}
 
 #carousel-1{
   position:relative;
@@ -113,20 +59,5 @@ export default{
 .carousel_img{
   height:580px;
 }
-/* media query*/
-@media (max-width: 1200px) {
-    .header_navabar{
-      padding: 10px 50px;
-    }
-    .carousel_img{
-      height:50%;
-    }
-  }
-    /* 
-@media (max-width: 992px) {
-  }
-@media (max-width: 576px) {
 
-  }
-  */
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,90 @@
+<template>
+        <div>
+          <b-navbar toggleable="lg" type="dark" variant="info" class="header_navabar">
+            <b-navbar-brand>
+              <h1>
+                <router-link to="/">Wine Space</router-link>
+              </h1>
+            </b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"><i class="fas fa-bars menu_color"></i></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+              <b-navbar-nav class="ml-auto navbar_right">
+                <b-nav-item right>
+                  <router-link to="/">首頁</router-link>
+                </b-nav-item>
+                <b-nav-item right>
+                  <router-link to="/products">商品</router-link>
+                </b-nav-item>
+                <b-nav-item right>
+                  <router-link to="/login" v-show="logInOrOut == false">Login</router-link>
+                  <p v-show="logInOrOut == true" @click="logOut">Logout</p>
+                </b-nav-item>
+              </b-navbar-nav>
+            </b-collapse>
+          </b-navbar>
+        </div>
+
+</template>
+
+<script>
+
+export default{
+  name:"Navbar",
+  computed:{
+    logInOrOut(){
+      return this.$store.state.signinChange;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header_navabar{
+  width:100%;
+  position:absolute;
+  top:0;
+  left:0;
+  z-index:2;
+  background-color:#494b50 !important;
+  padding: 10px 100px;
+  opacity:0.9;
+}
+.header_navabar a{
+  color:white;
+  text-decoration: none;
+}
+.header_navabar h1 a{
+  font-size:3rem;
+}
+.header_navabar button i{
+  font-size: 25px;
+  color: white;
+}
+
+.header_navabar .navbar_right a{
+  margin:0 20px;
+  font-size: 1.4rem;
+}
+#header a:hover{
+    font-weight:bold;
+}
+
+/* media query*/
+@media (max-width: 1200px) {
+    .header_navabar{
+      padding: 10px 50px;
+    }
+    .carousel_img{
+      height:50%;
+    }
+  }
+    /* 
+@media (max-width: 992px) {
+  }
+@media (max-width: 576px) {
+
+  }
+  */
+</style>
