05a5f928bd64d2848439e1cdee29e5dcc25cbb76
amber <amber20240@gmail.com>
2020-10-01 00:59:57 +0800

    update MainIntroductionbottom style

--- src/components/MainIntroductionbottom.vue
+++ src/components/MainIntroductionbottom.vue
@@ -2,28 +2,28 @@
     <div class="introduction_bottom">
       <b-row>
         <b-col class="introduction_bottom_div" cols="12 mb-5" xl="3" md="6 mb-0" sm="12">
-          <img src="https://i.imgur.com/74Ix92a.jpg" alt="" style="width:100%;height:500px;">
+          <img src="https://i.imgur.com/74Ix92a.jpg" alt="image">
           <div>
             <h5>Lorem</h5>
             <p>恆溫的寄送服務</p>
           </div>
         </b-col>
         <b-col class="introduction_bottom_div" cols="12 mb-5" xl="3" md="6 mb-0" sm="12">
-          <img src="https://i.imgur.com/7KWIixk.jpg" alt="" style="width:100%;height:500px;">
+          <img src="https://i.imgur.com/7KWIixk.jpg" alt="image">
           <div>
             <h5>Lorem</h5>
             <p>恆溫的寄送服務</p>
           </div>
         </b-col>
         <b-col class="introduction_bottom_div" cols="12 mb-5" xl="3" md="6 mb-0" sm="12">
-          <img src="https://i.imgur.com/7utTzva.jpg" alt="" style="width:100%;height:500px;">
+          <img src="https://i.imgur.com/7utTzva.jpg" alt="image">
           <div>
             <h5>Lorem</h5>
             <p>恆溫的寄送服務</p>
           </div>
         </b-col>
         <b-col class="introduction_bottom_div" cols="12 mb-5" xl="3" md="6 mb-0" sm="12">
-          <img src="https://i.imgur.com/64BxYpv.jpg" alt="" style="width:100%;height:500px;">
+          <img src="https://i.imgur.com/64BxYpv.jpg" alt="image">
           <div>
             <h5>Lorem</h5>
             <p>恆溫的寄送服務</p>
@@ -50,22 +50,10 @@ export default {
 }
 .introduction_bottom_div img{
   width:100%;
-  height:100%;
+  height:500px;
   display: block;
   position:relative;
 }
-/* 
-main .introduction .introduction_bottom div+div{
-  margin-left:25px;
-}
-
-main .introduction .introduction_bottom_img img{
-  display: block;
-  width:100%;
-  position:relative;
-}
-*/
-
 
 .introduction_bottom_div div{
   position:relative;
