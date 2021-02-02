8786de4978d64b8ff81cd6dcda2c7aba006e4700
amber <amber20240@gmail.com>
2021-02-02 22:44:24 +0800

    component - introduction  修改格式

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,40 +1,39 @@
 <template>
-      <div class="introduction">
-        <MainIntroductionTop></MainIntroductionTop>
-        <b-row class="justify-content-center">
-          <b-col cols="5">
-            <hr>
-          </b-col>
-        </b-row>
-        <MainIntroductionbottom></MainIntroductionbottom>
-      </div>
+  <div class="introduction">
+    <MainIntroductionTop></MainIntroductionTop>
+    <b-row class="justify-content-center">
+      <b-col cols="5">
+        <hr />
+      </b-col>
+    </b-row>
+    <MainIntroductionbottom></MainIntroductionbottom>
+  </div>
 </template>
 
 <script>
-import MainIntroductionTop from "@/components/MainIntroductionTop.vue"
-import MainIntroductionbottom from "@/components/MainIntroductionbottom.vue"
-
+import MainIntroductionTop from "@/components/MainIntroductionTop.vue";
+import MainIntroductionbottom from "@/components/MainIntroductionbottom.vue";
 
 export default {
-  name: 'App',
-  components:{
+  name: "App",
+  components: {
     MainIntroductionTop,
-    MainIntroductionbottom,
-  },
+    MainIntroductionbottom
   }
+};
 </script>
 
 
 <style scoped>
-main .introduction hr{
+main .introduction hr {
   background-color: #4f3608;
-  height:2px;
+  height: 2px;
   margin: 50px auto;
 }
-@media (min-width:576px) and (max-width:767px){
-  main .introduction hr{
+@media (min-width: 576px) and (max-width: 767px) {
+  main .introduction hr {
     margin: 25px auto;
   }
-  }
+}
 </style>
 
