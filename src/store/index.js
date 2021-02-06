045339896da9bf87ed11878bdf3faa954572aedc
amber <amber20240@gmail.com>
2021-02-06 20:07:28 +0800

    update vuex 格式 和 命名

--- src/store/index.js
+++ src/store/index.js
@@ -1,121 +1,119 @@
-import Vue from 'vue'
-import Vuex from "vuex"
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 Vue.use(Vuex);
-Vue.use(VueAxios, axios)
+Vue.use(VueAxios, axios);
 
 export default new Vuex.Store({
   state: {
-    productsPicks:"",
-    productsNews:"",
+    orgProductsPicks: "",
+    orgProductsNews: "",
     checkSignIn: false,
-    addCartClassic1:"",
-    addCartNews1:"",
-    classicProducts:"",
-    newProducts:"",
+    addCartClassic1: "",
+    addCartNews1: "",
+    classicProducts: "",
+    newProducts: ""
   },
   mutations: {
-    addCart1(state, id) {
-      if (state.productsPicks[id].total >= state.productsPicks[id].num) {
-        state.productsPicks[id].cart += state.productsPicks[id].num;
-      };
-      if (state.productsPicks[id].total >= state.productsPicks[id].num) {
-        state.productsPicks[id].total -= state.productsPicks[id].num;
-      };
-      if (state.productsPicks[id].total == 0) {
-        state.productsPicks[id].sale = "OUT OF STOCK";
-      }
+    getOrgProductsPicks(state, payload) {
+      const newPayload = payload.map(item => {
+        item.nums = 1;
+        return item;
+      });
+      state.orgProductsPicks = payload;
     },
-    addCart2(state, id) {
-      if (state.productsNews[id].total >= state.productsNews[id].num) {
-        state.productsNews[id].cart += state.productsNews[id].num;
-      };
-      if (state.productsNews[id].total >= state.productsNews[id].num) {
-        state.productsNews[id].total -= state.productsNews[id].num;
-      };
-      if (state.productsNews[id].total == 0) {
-        state.productsNews[id].sale = "OUT OF STOCK";
-      }
+    getOrgProductsNews(state, payload) {
+      const newPayload = payload.map(item => {
+        item.nums = 1;
+        return item;
+      });
+      state.orgProductsNews = payload;
     },
-    productsPicks(state, payload) {
-      let newPayload=[];
-      newPayload=payload.map((item)=>{
-                  item.nums=1;
-                  return item
-                });
-      state.productsPicks = newPayload;
-    },
-    productsNews(state, payload) {
-      let newPayload=[];
-      newPayload=payload.map((item)=>{
-                  item.nums=1;
-                  return item
-                });
-      state.productsNews = newPayload;
-    },
-    checkSignIn(state, payload){
+    checkSignIn(state, payload) {
       state.checkSignIn = payload;
     },
-    addCartClassic1(state, payload){
+    addCartClassic1(state, payload) {
       state.addCartClassic1 = payload;
     },
-    addCartNews1(state, payload){
+    addCartNews1(state, payload) {
       state.addCartNews1 = payload;
     },
-    classicProducts(state, payload){
+    classicProducts(state, payload) {
       state.classicProducts = payload;
     },
-    newProducts(state, payload){
+    newProducts(state, payload) {
       state.newProducts = payload;
     }
   },
-  actions:{
-    wineProductsPicks(context){
-      axios.get("https://vue-course-api.hexschool.io/api/wine5/products").then((res)=>{
-        context.commit("productsPicks",res.data.products);
-      })
+  actions: {
+    getOrgProductsPicks(context) {
+      axios
+        .get("https://vue-course-api.hexschool.io/api/wine5/products")
+        .then(res => {
+          context.commit("getOrgProductsPicks", res.data.products);
+        });
     },
-    wineProductsNews(context){
-      axios.get("https://vue-course-api.hexschool.io/api/wine52/products").then((res)=>{
-        context.commit("productsNews",res.data.products);
-      })
+    getOrgProductsNews(context) {
+      axios
+        .get("https://vue-course-api.hexschool.io/api/wine52/products")
+        .then(res => {
+          context.commit("getOrgProductsNews", res.data.products);
+        });
     },
-    addCartClassic(context,payload){
-      axios.post("https://vue-course-api.hexschool.io/api/wine5/cart",{data:payload}).then((res)=>{
-        context.commit("addCartClassic1",res.data);
-      })
+    addCartClassic(context, payload) {
+      axios
+        .post("https://vue-course-api.hexschool.io/api/wine5/cart", {
+          data: payload
+        })
+        .then(res => {
+          context.commit("addCartClassic1", res.data);
+        });
     },
-    addCartNews(context,payload){
-      axios.post("https://vue-course-api.hexschool.io/api/wine52/cart",{data:payload}).then((res)=>{
-        context.commit("addCartNews1",res.data.data);
-      })
+    addCartNews(context, payload) {
+      axios
+        .post("https://vue-course-api.hexschool.io/api/wine52/cart", {
+          data: payload
+        })
+        .then(res => {
+          context.commit("addCartNews1", res.data.data);
+        });
     },
-    currentClassicProducts(context){
-      axios.get("https://vue-course-api.hexschool.io/api/wine5/cart").then((res)=>{
-        context.commit("classicProducts",res.data.data.carts);
-      })
+    currentClassicProducts(context) {
+      axios
+        .get("https://vue-course-api.hexschool.io/api/wine5/cart")
+        .then(res => {
+          context.commit("classicProducts", res.data.data.carts);
+        });
     },
-    currentNewProducts(context){
-      axios.get("https://vue-course-api.hexschool.io/api/wine52/cart").then((res)=>{
-        context.commit("newProducts",res.data.data.carts);
-      })
+    currentNewProducts(context) {
+      axios
+        .get("https://vue-course-api.hexschool.io/api/wine52/cart")
+        .then(res => {
+          context.commit("newProducts", res.data.data.carts);
+        });
     },
-    delectProductsClassic(context,payload){
-      axios.delete(`https://vue-course-api.hexschool.io/api/wine5/cart/${payload}`).then((res)=>{
-        context.dispatch("currentClassicProducts");
-      })
+    delectProductsClassic(context, payload) {
+      axios
+        .delete(`https://vue-course-api.hexschool.io/api/wine5/cart/${payload}`)
+        .then(res => {
+          context.dispatch("currentClassicProducts");
+        });
     },
-    delectProductsNew(context,payload){
-      axios.delete(`https://vue-course-api.hexschool.io/api/wine52/cart/${payload}`).then((res)=>{
-        context.dispatch("currentNewProducts");
-      })
+    delectProductsNew(context, payload) {
+      axios
+        .delete(
+          `https://vue-course-api.hexschool.io/api/wine52/cart/${payload}`
+        )
+        .then(res => {
+          context.dispatch("currentNewProducts");
+        });
     },
-    signOutChange(context){
-      axios.post("https://vue-course-api.hexschool.io/logout").then((result)=>{
-        context.commit("checkSignIn",false);
-      })
+    signOutChange(context) {
+      axios.post("https://vue-course-api.hexschool.io/logout").then(result => {
+        context.commit("checkSignIn", false);
+      });
     }
   }
-})
+});
