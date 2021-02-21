619af1e0ca2abe683e460a15050a8d325ff01b32
amber <amber20240@gmail.com>
2021-02-21 23:04:33 +0800

    新增 views - 結帳page - 確認購物車 component - 完成2/3

--- src/components/CheckoutShoppingCartContent.vue
+++ src/components/CheckoutShoppingCartContent.vue
@@ -1,12 +1,123 @@
 <template>
-  <div>
+  <div class="container">
+    <div
+      v-for="(category, index) in categoryProducts"
+      :key="index"
+    >
+      <h1>{{ category.title }}</h1>
+      <table>
+        <tr>
+          <th>品名</th>
+          <th>數量</th>
+          <th>單價</th>
+          <th>總額</th>
+        </tr>
+        <tr
+          v-for="(product, index) in category.data"
+          :key="index"
+        >
+          <td>{{ product.product.title }}</td>
+          <td>{{ product.qty }}瓶</td>
+          <td>{{ product.product.price }}</td>
+          <td>${{ product.total }}</td>
+        </tr>
+      </table>
+    </div>
+    <div class="total">
+      <h1>total</h1>
+      <div class="totalContent">
+        <span>總計 : </span>
+        <span>{{ totalPrice }}</span>
+      </div>
+    </div>
   </div>
 </template>
+
 <script>
 export default {
   name: 'CheckoutShoppingCartContent',
+  props: ['classicProductData', 'newProductData'],
+  data () {
+    return {
+      classicData: {},
+      newData: {},
+      classicAndNewData: []
+    }
+  },
+  computed: {
+    categoryProducts () {
+      return this.getCategoryProducts()
+    },
+    totalPrice () {
+      return this.getTotalPrice()
+    }
+  },
+  watch: {
+    classicProductData () {
+      // eslint-disable-next-line dot-notation
+      this.classicData['title'] = 'classic(經典款)'
+      // eslint-disable-next-line dot-notation
+      this.classicData['data'] = this.classicProductData
+      this.classicAndNewData.push(this.classicData)
+    },
+    newProductData () {
+      // eslint-disable-next-line dot-notation
+      this.newData['title'] = 'new(新款)'
+      // eslint-disable-next-line dot-notation
+      this.newData['data'] = this.newProductData
+      this.classicAndNewData.push(this.newData)
+    }
+  },
+  methods: {
+    getCategoryProducts () {
+      return this.classicAndNewData
+    },
+    getTotalPrice () {
+      const tempArray = []
+      this.classicProductData.forEach(item => tempArray.push(item.total))
+      this.newProductData.forEach(item => tempArray.push(item.total))
+      const total = tempArray.reduce((prev, currentItem, currentItemIndex, array) => { return prev + currentItem }, 0)
+      return total
+    }
+  }
 }
 </script>
 
 <style scoped>
+.container {
+  width: 85%;
+  margin: auto;
+}
+table {
+  width: 100%;
+}
+h1 {
+  font-size: 1.5rem;
+  margin: 25px 0;
+}
+th, td {
+  width: 25%;
+  text-align: center;
+}
+tr {
+  line-height: 2.5;
+}
+th {
+  background-color: bisque;
+}
+td {
+  border-bottom: 1px solid #7B7B7B ;
+}
+.total {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 25px 0;
+}
+.total h1 {
+  margin: 0;
+}
+.totalContent span{
+  font-size: 1rem;
+}
 </style>
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -10,17 +10,26 @@
           v-for="(item, index) in checkoutSop"
           :key="index"
           class="checkout_sop_item"
+          :class="{bgColor: changeTab === item.component}"
         >
           <h2> {{item.title}} </h2>
         </div>
-        <component :is="changeTab"></component>
       </div>
+      <div class="checkout_sop_content">
+        <component
+            :is="changeTab"
+            :classicProductData = classicProductData
+            :newProductData = newProductData
+          ></component>
+      </div>
+
     </div>
   </div>
 </template>
 <script>
 import Navbar from '@/components/Navbar.vue'
 import CheckoutShoppingCartContent from '@/components/CheckoutShoppingCartContent.vue'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'Checkout',
@@ -37,17 +46,27 @@ export default {
           component: 'CheckoutShoppingCartContent'
         },
         {
-          title: '2.輸入收件人資料',
+          title: '2.輸入收件人資料'
           // component: 'CheckoutShoppingCartContent'
         },
         {
-          title: '3.訂單完成',
+          title: '3.訂單完成'
           // component: 'CheckoutShoppingCartContent'
         }
       ]
     }
   },
   computed: {
+    ...mapState([
+      'currentShoppingCartClassic',
+      'currentShoppingCartNew'
+    ]),
+    classicProductData () {
+      return this.currentShoppingCartClassic
+    },
+    newProductData () {
+      return this.currentShoppingCartNew
+    },
     changeTab () {
       return this.componentTab
     },
@@ -56,30 +75,39 @@ export default {
     }
   },
   mounted () {
-    this.getOrgProductsClassic()
-    this.getOrgProductsNews()
     this.getCurrentShoppingCartClassic()
     this.getCurrentShoppingCartNew()
+  },
+  methods: {
+    ...mapActions([
+      'getCurrentShoppingCartClassic',
+      'getCurrentShoppingCartNew'
+    ])
   }
 }
 </script>
 
 <style scoped>
+* {
+  margin-bottom: 0;
+}
 .container {
   max-width: 1140px;
-  padding: 0 50px;
+  padding: 0 48px;
 }
 .nav_bar_root {
-  height: 50px;
+  height: 48px;
 }
 h1 {
   color: #7c757d;
   font-size: 1.5rem;
   text-align: center;
+  margin: 25px 0;
 }
 .checkout_sop {
   display: flex;
   justify-content: center;
+  margin: 25px 0;
 }
 .checkout_sop div+div {
   margin-left: 35px;
@@ -89,5 +117,10 @@ h1 {
 }
 .checkout_sop_item h2{
   font-size: 1rem;
+  font-weight: 400;
+}
+.bgColor {
+  background-color:bisque;
+  border-radius: 5px;
 }
 </style>
