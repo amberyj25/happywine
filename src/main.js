9fd4b78f28f9dfaf4ab91cf9d38ce1ef71937925
amber <amber20240@gmail.com>
2021-02-23 09:34:55 +0800

    update eslint bug

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import Loading from 'vue-loading-overlay';
-import 'vue-loading-overlay/dist/vue-loading.css';
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
 
 import App from './App'
 
