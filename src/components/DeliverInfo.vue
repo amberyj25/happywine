112bb09df56b8044880422cb6eaf3f12748fbbfe
amber <amber20240@gmail.com>
2021-02-26 22:12:43 +0800

    改寫成SCSS - component - DeliverInfo

--- src/components/DeliverInfo.vue
+++ src/components/DeliverInfo.vue
@@ -10,8 +10,8 @@
         md="6 mb-0"
         sm="12"
       >
-        <img :src="deliver.img" alt="image" />
-        <div>
+        <img :src="deliver.img" alt="image" class="img"/>
+        <div class="text">
           <h5>{{ deliver.title }}</h5>
           <p>{{ deliver.description }}</p>
         </div>
@@ -52,33 +52,31 @@ export default {
 }
 </script>
 
-<style scoped>
-.deliver_info div {
+<style lang="scss" scoped>
+/* .deliver_info div {
   justify-content: center;
-}
+} */
 
 .content {
   width: 100%;
   height: 500px;
-}
-.content img {
-  width: 100%;
-  height: 500px;
-  object-fit: cover;
-  display: block;
-  position: relative;
-}
-
-.content div {
-  position: relative;
-  left: 0;
-  bottom: 55px;
-  background-color: #8c6e55;
-  color: white;
-  opacity: 0.8;
-}
-.content h5,
-.content p {
-  text-align: center;
+  img {
+    width: 100%;
+    height: 500px;
+    object-fit: cover;
+    display: block;
+    position: relative;
+  }
+  .text {
+    position: relative;
+    left: 0;
+    bottom: 55px;
+    background-color: #8c6e55;
+    color: white;
+    opacity: 0.8;
+    h5, p {
+      text-align: center;
+    }
+  }
 }
 </style>
